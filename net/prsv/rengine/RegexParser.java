b1cbda2b0feae092addea3e836d7aa8ca2aa9eca
Pavel Urusov <pavel.urusov@gmail.com>
2023-08-20 10:53:05 +0100

    RegexParser: all methods are now static

--- net/prsv/rengine/RegexParser.java
+++ net/prsv/rengine/RegexParser.java
@@ -6,47 +6,39 @@ import net.prsv.rengine.RToken.RTokenType;
 
 public class RegexParser {
 
-    private final String pattern;
-    private static final Map<RTokenType, Integer> precedence;
+    private static final Map<RTokenType, Integer> precedence = new HashMap<>();
 
-    private static final Map<Character, RTokenType> specialChars;
-
-    private List<RToken> tokens = new ArrayList<>();
+    private static final Map<Character, RTokenType> specialChars = new HashMap<>();
 
     static {
-        Map<RTokenType, Integer> tokenPrecedenceMap = new HashMap<>();
-        tokenPrecedenceMap.put(RTokenType.LPAR, 1);
-        tokenPrecedenceMap.put(RTokenType.UNION, 2);
-        tokenPrecedenceMap.put(RTokenType.CONCAT, 3);
-        tokenPrecedenceMap.put(RTokenType.Q, 4);
-        tokenPrecedenceMap.put(RTokenType.STAR, 4);
-        tokenPrecedenceMap.put(RTokenType.PLUS, 4);
-        precedence = Collections.unmodifiableMap(tokenPrecedenceMap);
+        // populate the precedence map
+        precedence.put(RTokenType.LPAR, 1);
+        precedence.put(RTokenType.UNION, 2);
+        precedence.put(RTokenType.CONCAT, 3);
+        precedence.put(RTokenType.Q, 4);
+        precedence.put(RTokenType.STAR, 4);
+        precedence.put(RTokenType.PLUS, 4);
 
-        Map<Character, RTokenType> specialCharMap = new HashMap<>();
-        specialCharMap.put('(', RTokenType.LPAR);
-        specialCharMap.put(')', RTokenType.RPAR);
-        specialCharMap.put('?', RTokenType.Q);
-        specialCharMap.put('*', RTokenType.STAR);
-        specialCharMap.put('+', RTokenType.PLUS);
-        specialCharMap.put('|', RTokenType.UNION);
-        specialChars = Collections.unmodifiableMap(specialCharMap);
-
+        // populate the special character map
+        specialChars.put('(', RTokenType.LPAR);
+        specialChars.put(')', RTokenType.RPAR);
+        specialChars.put('?', RTokenType.Q);
+        specialChars.put('*', RTokenType.STAR);
+        specialChars.put('+', RTokenType.PLUS);
+        specialChars.put('|', RTokenType.UNION);
     }
 
     // do not instantiate
-    public RegexParser (String pattern) {
+    private RegexParser () {
+    }
+
+    public static List<RToken> tokenize(String pattern) {
+
         if (pattern == null) {
             throw new IllegalArgumentException("Regex parser: pattern can't be null");
         }
-        this.pattern = pattern;
-        tokenize();
-        inToPost();
-    }
 
-    private void tokenize() {
-
-        ArrayList<RToken> tokenStream = new ArrayList<>();
+        ArrayList<RToken> temporaryTokenStream = new ArrayList<>();
 
         int position = 0;
 
@@ -73,24 +65,29 @@ public class RegexParser {
                 // if not a special character -- create a literal token
                 token = new RToken(specialChars.getOrDefault(c, RTokenType.LITERAL), c);
             }
-            tokenStream.add(token);
+            temporaryTokenStream.add(token);
             position = position + 1;
         }
 
+        List<RToken> tokens = new ArrayList<>();
+
         // insert explicit concatenation tokens
-        for (int i = 0; i < tokenStream.size(); i++) {
-            RToken t = tokenStream.get(i);
+        for (int i = 0; i < temporaryTokenStream.size(); i++) {
+            RToken t = temporaryTokenStream.get(i);
             tokens.add(t);
-            if (i + 1 < tokenStream.size()) {
-                RToken t2 = tokenStream.get(i + 1);
-                if (t.type != RTokenType.LPAR && t.type != RTokenType.UNION && t2.type == RTokenType.LITERAL) {
-                    tokens.add(new RToken(RTokenType.CONCAT, '.'));
+            if (i + 1 < temporaryTokenStream.size()) {
+                RToken t2 = temporaryTokenStream.get(i + 1);
+                if (t.type != RTokenType.LPAR && t.type != RTokenType.UNION && (t2.type == RTokenType.LITERAL || t2.type == RTokenType.LPAR)) {
+                    tokens.add(new RToken(RTokenType.CONCAT, '&'));
                 }
             }
         }
+
+        return tokens;
+
     }
 
-    private void inToPost() {
+    public static List<RToken> infixToPostfix(List<RToken> tokens) {
         Deque<RToken> stack = new ArrayDeque<>();
         List<RToken> postfixStream = new ArrayList<>();
 
@@ -128,13 +125,8 @@ public class RegexParser {
         while (stack.size() > 0) {
             postfixStream.add(stack.pop());
         }
-        tokens = postfixStream;
+        return postfixStream;
     }
-
-    public List<RToken> tokenStream() {
-        return tokens;
-    }
-
 
 }
 
