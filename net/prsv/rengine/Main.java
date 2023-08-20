cd09176af4a0f07b1b19d8fc6ca26de65a3e7ab3
Pavel Urusov <pavel.urusov@gmail.com>
2023-08-20 11:17:18 +0100

    driver class to showcase the pattern matching

--- net/prsv/rengine/Main.java
+++ net/prsv/rengine/Main.java
@@ -1,8 +1,35 @@
 package net.prsv.rengine;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String pattern = "ab*c|ca*bd";
-        RegexParser rp = new RegexParser(pattern);
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the regular expression: ");
+        System.out.print("> ");
+        String pattern = s.nextLine();
+        StateMachine machine = NFABuilder.build(RegexParser.infixToPostfix(RegexParser.tokenize(pattern)));
+
+
+        boolean quit = false;
+
+        while(!quit) {
+            System.out.println("Enter the input string, or type ':QUIT' to exit: ");
+            System.out.print("> ");
+            String input = s.nextLine();
+            if(input.trim().equalsIgnoreCase(":quit")) {
+                quit = true;
+                System.out.println("Bye!");
+                continue;
+            }
+            boolean accepted = machine.run(input);
+            if (accepted) {
+                System.out.println("String '" + input + "' accepted.");
+            } else {
+                System.out.println("String '" + input + "' rejected.");
+            }
+        }
+        s.close();
+
     }
 }
